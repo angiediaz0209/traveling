c5ed25c5aaf510a8923914d1f639da70f064545f
angiediaz0209 <83033853+angiediaz0209@users.noreply.github.com>
2022-04-16 21:40:52 -0700

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -12,13 +12,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='https://angiediaz0209.github.io/traveling/images/img-9.jpg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='https://angiediaz0209.github.io/traveling/images/img-2.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -26,19 +26,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='https://angiediaz0209.github.io/traveling/images/img-3.jpg'
               text='Set Sailing the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='https://angiediaz0209.github.io/traveling/images/img-4.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='https://angiediaz0209.github.io/traveling/images/img-8.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
